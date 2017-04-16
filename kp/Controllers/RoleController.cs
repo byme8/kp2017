b8abd53ec39b1fad0878168cda763be18a382455
Stanislav Silin <byme.work@gmail.com>
2017-04-16 12:14:51 +0300

    Make role controller available only for admins

--- kp/Controllers/RoleController.cs
+++ kp/Controllers/RoleController.cs
@@ -1,10 +1,12 @@
 ﻿using kp.Business.Abstractions.Services;
 using kp.Domain.Data;
 using kp.WebApi.Controllers.Core;
+using kp.WebApi.Filters;
 using Microsoft.AspNetCore.Mvc;
 
 namespace kp.WebApi.Controllers
 {
+    [OnlyAdmins]
     [Route("api/users/roles")]
     public class RoleController : EntityController<Role>
     {
